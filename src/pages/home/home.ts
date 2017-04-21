bd084739b9355de1acc768064d4413064ae205f2
Lucas Adams <adams.lucass@gmail.com>
2017-04-21 15:50:19 -0300

    primeiro commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from '@angular/core';
+import {NavController, LoadingController, AlertController} from 'ionic-angular';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage implements OnInit {
+
+  public carros;
+
+  constructor(
+    public navCtrl: NavController,
+    private _http: Http,
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController) {
+
+  }
+
+  ngOnInit(): void {
+    let loader = this._loadingCtrl.create({
+      content: 'Buscando novos carros. Aguarde ...'
+    });
+    loader.present();
+
+    this._http
+      .get('http://aluracar.herokuapp.com')
+      .map(res => res.json())
+      .toPromise()
+      .then(carros => {
+        this.carros = carros;
+        loader.dismiss();
+      })
+      .catch(err => {
+        console.error(err);
+        loader.dismiss();
+        this._alertCtrl.create({
+          title: 'Falha na conexão',
+          buttons: [{ text: 'Estou ciente!'}],
+          subTitle: 'Não foi possível obter a lista de carros. Tente mais tarde.'
+        }).present();
+      });
+  }
+
+}
